1daf83cfe300424e8ac42a84ea52276c4a120e1c
noname <l2c@qq.com>
2016-06-18 09:56:52 +0800

    增加if while
    增加if while

--- ast.c
+++ ast.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
 #include "ast.tab.h"
 struct ast *
 	newast(int nodetype, struct ast *l, struct ast *r)
@@ -45,9 +47,48 @@ double eval(struct ast *a) {
 		v = eval(a->l) * eval(a->r); break;
 	case '/':
 		v = eval(a->l) / eval(a->r); break;
+	case '|':
+		v = fabs(eval(a->l)); break;
 	case 'M':
 		v = -eval(a->l);
 		break;
+	case '1': v = (eval(a->l) > eval(a->r)) ? 1 : 0; break;
+	case '2': v = (eval(a->l) < eval(a->r)) ? 1 : 0; break;
+	case '3': v = (eval(a->l) != eval(a->r)) ? 1 : 0; break;
+	case '4': v = (eval(a->l) == eval(a->r)) ? 1 : 0; break;
+	case '5': v = (eval(a->l) >= eval(a->r)) ? 1 : 0; break;
+	case '6': v = (eval(a->l) <= eval(a->r)) ? 1 : 0; break;
+	case 'I':
+	{
+		struct flow* f = (struct flow*)a;
+		if (eval(f->cond) != 0) {
+			if (f->tl) {
+				v = eval(f->tl);
+			}
+			else {
+				v = 0;
+			}
+		}
+		else {
+			if (f->el) {
+				v = eval(f->el);
+			}
+			else {
+				v = 0;
+			}
+		}
+		break;
+	}
+	case 'W':
+	{
+		struct flow *f = (struct flow*)a;
+		if (f->tl) {
+			while (eval(f->cond) != 0) {
+				v = eval(f->tl);
+			}
+		}
+		break;
+	}
 	default:
 		printf("internal error bad node %c \n", a->nodetype);
 	}
@@ -61,16 +102,36 @@ void treefree(struct ast *a) {
 	case '+':
 	case '-':
 	case '*':
+	case '/':
+	case '1':
+	case '2':
+	case '3':
+	case '4':
+	case '5':
+	case '6':
 		treefree(a->r);
 	case '|':
 	case 'M':
 		treefree(a->l);
-	case 'K':
-		free(a);
+	case 'K': case 'N':
+		break;
+	case '=':
+		free( ((struct symasgn*)a)->v);
+		break;
+	case 'I': case 'W':
+		treefree(((struct flow *)a)->cond);
+		if (((struct flow*)a)->tl) {
+			treefree( ((struct flow*)a)->tl);
+		}
+		if (((struct flow*)a)->el) {
+			treefree(((struct flow*)a)->el);
+		}
 		break;
 	default:
 		printf("internal error: free bad node %c\n", a->nodetype);
 	}
+
+	free(a);
 }
 
 void
@@ -91,6 +152,115 @@ static unsigned symhash(char *sym) {
 	return hash;
 }
 
-struct symlist *newsymlist(struct symbol *sym, struct symlist *next) {
+struct symbol* lookup(char *sym) {
+	struct symbol *sp = &symtab[symhash(sym)];
+	int scount = NHASH;
+	while (--scount >= 0) {
+		if (sp->name && !strcmp(sp->name, sym)) {
+			return sp;
+		}
+		if (!sp->name) {
+			sp->name = strdup(sym);
+			sp->value = 0;
+			sp->func = NULL;
+			sp->syms = NULL;
+			return sp;
+		}
+		if (++sp >= symtab + NHASH) sp = symtab;
+	}
+	yyerror("symbol table overflow\n");
+	abort();
 
 }
+
+struct ast* newcmp(int cmptype, struct ast *l, struct ast *r) {
+	struct ast *a = malloc(sizeof(struct ast));
+	if (!a) {
+		yyerror("create new cmp error");
+		abort();
+	}
+	a->nodetype = '0' + cmptype;
+	a->l = l;
+	a->r = r;
+	return a;
+}
+struct symlist *newsymlist(struct symbol *sym, struct symlist *next) {
+	struct symlist *s = malloc(sizeof(struct symlist));
+	if (!s) {
+		yyerror("create new symlist error");
+		abort();
+	}
+	s->sym = sym;
+	s->next = next;
+	return s;
+}	
+
+struct ast *newfunc(int functype, struct ast *l) {
+	struct fncall *f = malloc(sizeof(struct fncall));
+	if (!f) {	
+		yyerror("create new func error");
+		abort();
+	}
+	f->nodetype = 'F';
+	f->l = l;
+	f->functype = functype;
+	return (struct ast*)f;
+}
+
+struct ast *newcall(struct symbol *s, struct ast *l) {
+	struct ufncall *f = malloc(sizeof(struct ufncall));
+	if (!f) {
+		yerror("create new func error");
+		abort();
+	}
+	f->nodetype = 'C';
+	f->s = s;
+	f->l = l;
+	return (struct ast*)f;
+}
+
+struct ast *newref(struct symbol *s) {
+	struct symref *ref = malloc(sizeof(struct symref));
+	if (!ref) {
+		yerror("create new ref error");
+		abort();
+	}
+	ref->nodetype = 'N';
+	ref->s = s;
+	return (struct ast*)ref;
+}
+
+struct ast *newasgn(struct symbol *s, struct ast *v) {
+	struct symasgn *a = malloc(sizeof(struct symasgn));
+	if (!a) {
+		yerror("create asgn error");
+		absort();
+	}
+	a->nodetype = '=';
+	a->s = s;
+	a->v = v;
+	return (struct ast*)a;
+}
+
+struct ast *newflow(int nodetype, struct ast *cond, struct ast* tl, struct ast *el) {
+	struct flow *f = malloc(sizeof(struct flow));
+	if (!f) {
+		yerror("create flow error");
+		absort();
+	}
+	f->nodetype = nodetype;
+	f->cond = cond;
+	f->tl = tl;
+	f->el = el;
+	return (struct ast*)f;
+}
+
+void symlistfree(struct symlist *sl) {
+	struct symlist *nsl;
+	while (sl) {
+		nsl = sl->next;
+		free(sl);
+		sl = nsl;
+	}
+}
+
